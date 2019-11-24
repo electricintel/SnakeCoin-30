925640ad45a04b213880f9d4a2a5bf7495960cf4
xsqrenda <xsqrenda@qq.com>
2019-11-24 10:47:02 +0800

    修正test1.py

--- test1.py
+++ test1.py
@@ -2,8 +2,8 @@ from nextBlock import *
 from initBlock import *
 
 # Create the blockchain and add the genesis block
-blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+block_chain = [create_genesis_block()]
+previous_block = block_chain[0]
 
 # How many blocks should we add to the chain
 # after the genesis block
@@ -12,7 +12,7 @@ num_of_blocks_to_add = 2
 # Add blocks to the chain
 for i in range(0, num_of_blocks_to_add):
     block_to_add = next_block(previous_block)
-    blockchain.append(block_to_add)
+    block_chain.append(block_to_add)
     previous_block = block_to_add
     # Tell everyone about it!
     print("Block #{} has been added to the blockchain!".format(block_to_add.index))
